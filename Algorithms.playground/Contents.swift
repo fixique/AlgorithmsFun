8a34e65ee68e174e2dd32ab9b14f0fc38e015669
Vlad Krupenko <dev.fixique@gmail.com>
2017-04-09 23:44:59 +0300

    Add Insertion Sort

--- Algorithms.playground/Contents.swift
+++ Algorithms.playground/Contents.swift
@@ -157,3 +157,44 @@ q.count
 q.enqueue("Grace")
 q.array
 q.count
+
+
+// Insertion Sort 
+
+func insertionSort(_ array: [Int]) -> [Int] {
+    var a = array
+    for x in 1..<a.count {
+        var y = x
+        let temp = a[y]
+        while y > 0 && temp < a[y - 1] {
+            a[y] = a[y - 1]
+            y -= 1
+        }
+        a[y] = temp
+    }
+    
+    return a
+}
+
+
+let list = [10, 1, 2, 4, 5, 3, 4, 10, 41, 34, -1 , -12]
+insertionSort(list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
